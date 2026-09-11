069a9ad2ce42b237888e9609a6ec75999d61aae9
IsaacEffect <isaacmartinez9875@gmail.com>
2026-09-11 12:03:28 -0400

    arreglando datos

--- VisorDeDocumentos/obj/Debug/net8.0/VisorDeDocumentos.AssemblyInfo.cs
+++ VisorDeDocumentos/obj/Debug/net8.0/VisorDeDocumentos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisorDeDocumentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81a2f688f34f6bef8a826d8d01623c72294d6dcd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85598fceec4c9e965df41d978181c34c2fc7c531")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisorDeDocumentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VisorDeDocumentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
